7080c67eaf2fd2d22c0bcde9bc14120d1ef6b721
Serge <iefserge@runtimejs.org>
2016-01-03 21:37:13 +0200

    Remove unused dependency

--- index.js
+++ index.js
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var path = require('path');
-
 module.exports = function(package) {
   if (typeof package.name !== 'string') {
     throw new Error('unknown package name');
